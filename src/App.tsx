1af050adcefc613b311a50ffe15043415a918c1a
martinmccallumwilliams-design <martin@test.com>
2026-03-03 17:16:36 +0000

    Remove scroll indicator from hero section

--- src/App.tsx
+++ src/App.tsx
@@ -424,17 +424,6 @@ export default function App() {
             </div>
           </motion.div>
         </div>
-
-        {/* Scroll Indicator */}
-        <motion.div
-          animate={{ y: [0, 10, 0] }}
-          transition={{ repeat: Infinity, duration: 2 }}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2 text-gray-600"
-        >
-          <div className="w-6 h-10 border-2 border-gray-600 rounded-full flex justify-center pt-2">
-            <div className="w-1 h-2 bg-gray-600 rounded-full" />
-          </div>
-        </motion.div>
       </section>
 
       {/* Series Overview */}
